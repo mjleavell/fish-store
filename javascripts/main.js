d8f27306df85d5a1097ad0f1698bb3cf7770ea13
Maggie Leavell <mjleavell@gmail.com>
2018-10-15 21:27:04 -0500

    console log fish array

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,3 +2,11 @@
 
 // Add fish to "Basket"
 
+// Load Fish
+$.get('../db/fishes.json')
+    .done((data) => {
+        console.log(data);
+    })
+    .fail((error) => {
+        console.log(error);
+    });
